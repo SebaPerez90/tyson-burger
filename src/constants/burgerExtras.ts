ede61174c23207bc7b461c51bff92a3b20ff27df
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2025-11-16 16:42:08 -0300

    updates

--- src/constants/burgerExtras.ts
+++ src/constants/burgerExtras.ts
@@ -1,15 +1,15 @@
 export const burgerExtras = [
-  { id: "extra-meat", label: "Extra medallon de carne", price: "$ 3.000" },
-  { id: "extra-bacon", label: "Doble bacon extra", price: "$ 2.000" },
+  { id: "extra-meat", label: "Extra medallon de carne", price: "$ 4.000" },
+  { id: "extra-bacon", label: "Doble bacon extra", price: "$ 2.500" },
+  { id: "extra-cheese", label: "Doble cheddar al medallon", price: "$ 600" },
   {
     id: "extra-cheese-fries",
     label: "Extra cheddar a tus papas",
-    price: "$ 2.000",
+    price: "$ 2.500",
   },
   {
     id: "extra-cheese-bacon-fries",
     label: "Extra cheddar y bacon a tus papas",
     price: "$ 3.500",
   },
-  { id: "extra-cheese", label: "Extra cheddar al medallon", price: "$ 500" },
 ];

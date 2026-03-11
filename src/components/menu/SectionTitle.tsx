61a49aa6f8ec1972ac895afc0f31ef5ab28da52a
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2026-03-11 18:43:46 -0300

    updates

--- src/components/menu/SectionTitle.tsx
+++ src/components/menu/SectionTitle.tsx
@@ -8,7 +8,7 @@ const SectionTitle = ({
   return (
     <header className="flex flex-col items-center justify-center gap-1 pb-5 ">
       <h2 className="text-md sm:text-lg font-bold text-white">{title} </h2>
-      <span className="text-neutral-200 text-[8px] font-normal">
+      <span className="text-neutral-200 text-[8px] sm:text-xs font-normal">
         {subtitle}
       </span>
     </header>

79cd75935a1b96bca4b0138a8de6d954bb5bd7d2
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2026-04-09 21:23:27 -0300

    updates

--- src/constants/burgerExtras.ts
+++ src/constants/burgerExtras.ts
@@ -9,34 +9,29 @@ export const burgerExtras = [
     label: 'Agregá bastones de mozzarella',
     price: '$ 7.000',
   },
-  {
-    id: 'extra-cheddar-fuente',
-    label: 'Fuente Cheddar y Bacon',
-    price: '$ 5.000',
-  },
+  // {
+  //   id: 'extra-cheddar-fuente',
+  //   label: 'Fuente Cheddar y Bacon',
+  //   price: '$ 5.000',
+  // },
   // {
   //   id: 'extra-sticks',
   //   label: 'Triangulitos de provoleta frita',
   //   price: '$ 6.000',
   // },
   {
-    id: 'extra-nuggets',
-    label: 'Agregá patitas de pollo',
-    price: '$ 5.000',
-  },
-  {
     id: 'extra-xl-fries',
     label: 'Papas grandes',
     price: '$ 3.000',
   },
-  {
-    id: 'extra-cheese-fries',
-    label: 'Papas grandes + Cheddar',
-    price: '$ 4.000',
-  },
-  {
-    id: 'extra-cheese-bacon-fries',
-    label: 'Papas grandes + Cheddar + Bacon',
-    price: '$ 5.000',
-  },
+  // {
+  //   id: 'extra-cheese-fries',
+  //   label: 'Papas grandes + Cheddar',
+  //   price: '$ 4.000',
+  // },
+  // {
+  //   id: 'extra-cheese-bacon-fries',
+  //   label: 'Papas grandes + Cheddar + Bacon',
+  //   price: '$ 5.000',
+  // },
 ];

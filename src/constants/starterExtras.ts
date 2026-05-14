d9b59f65b1dff6926e81f18fd8f7580167c69d58
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2026-05-14 10:12:32 -0300

    updates

--- src/constants/starterExtras.ts
+++ src/constants/starterExtras.ts
@@ -1,14 +1,20 @@
 export const starterExtras = [
-  { id: 'extra-cheese-fries', label: 'Dip cheddar XL', price: '$ 4.000' },
+  {
+    id: 'extra-cheese-fries',
+    label: 'Dip cheddar XL',
+    price: '$ 4.000',
+    img: '',
+  },
   {
     id: 'extra-cheddar-fuente',
     label: 'Fuente de cheddar y bacon',
     price: '$ 7.000',
+    img: '',
   },
 ];
 
 export const saucesExtras = [
-  { id: 'extra-mayonnaise', label: 'Mayonesa', price: '0' },
-  { id: 'extra-bbq', label: 'Barbacoa', price: '0' },
-  { id: 'extra-moztaza', label: 'Moztaza', price: '0' },
+  { id: 'extra-mayonnaise', label: 'Mayonesa', price: '0', img: '' },
+  { id: 'extra-bbq', label: 'Barbacoa', price: '0', img: '' },
+  { id: 'extra-moztaza', label: 'Moztaza', price: '0', img: '' },
 ];

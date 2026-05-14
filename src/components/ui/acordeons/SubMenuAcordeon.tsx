00bd8d3b313c233aa97fceab71ce73fb2686ef48
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2026-05-14 10:50:18 -0300

    updates

--- src/components/ui/acordeons/SubMenuAcordeon.tsx
+++ src/components/ui/acordeons/SubMenuAcordeon.tsx
@@ -50,7 +50,7 @@ const SubMenuAcordeon = ({
                         id={item.id}
                         checked={isSelected}
                         onCheckedChange={() => onExtraChange(item)}
-                        className='h-5 w-5 sm:w-6 sm:h-6 rounded-none sm:rounded-sm bg-zinc-800 border-white/30 cursor-pointer data-[state=checked]:bg-red-500 data-[state=checked]:border-red-400 data-[state=checked]:text-white'
+                        className='w-8 h-8 rounded-none sm:rounded-sm bg-zinc-800 border-white/30 cursor-pointer data-[state=checked]:bg-red-500 data-[state=checked]:border-red-400 data-[state=checked]:text-white'
                       />
 
                       <div className='ml-2 flex flex-col gap-2 items-start min-w-[170px]'>
@@ -63,10 +63,10 @@ const SubMenuAcordeon = ({
                         <Image
                           src={item.img}
                           alt={item.label}
-                          width={80}
-                          height={80}
+                          width={90}
+                          height={90}
                           onClick={() => setSelectedImage(item)}
-                          className='w-20 h-20 object-cover rounded-md cursor-pointer transition duration-300 hover:scale-105'
+                          className='w-30 h-25 object-cover rounded-md cursor-pointer transition duration-300 hover:scale-105'
                         />
                       </div>
                     </div>
